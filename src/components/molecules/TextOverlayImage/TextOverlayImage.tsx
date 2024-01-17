9d28be802206cf8bbc39e09320c13a1910014f76
Tomasz Stefaniuk <41304693+esemku@users.noreply.github.com>
2024-01-17 07:58:24 +0100

    feat: add salons section

---
+++ src/components/molecules/TextOverlayImage/TextOverlayImage.tsx
@@ -0,0 +1,104 @@
+import {
+  Box,
+  Button,
+  SxProps,
+  Theme,
+  Typography,
+  useMediaQuery,
+} from '@mui/material';
+import { FC } from 'react';
+
+type Props = {
+  fromSide: 'left' | 'right';
+  imageSrc: string;
+  title: string;
+  description: string;
+  buttonTitle: string;
+  sx?: SxProps;
+};
+
+export const TextOverlayImage: FC<Props> = ({
+  fromSide = 'left',
+  imageSrc,
+  title,
+  description,
+  buttonTitle,
+}) => {
+  const isMobile = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.down('md')
+  );
+
+  let flexDirection = fromSide === 'right' ? 'row' : 'row-reverse';
+  let left: number | string = -80;
+  let right = fromSide === 'right' ? 'unset' : -80;
+  if (isMobile) {
+    flexDirection = 'column';
+    left = fromSide === 'left' ? 'unset' : 0;
+    right = fromSide === 'right' ? 'auto' : 0;
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection,
+        paddingY: 16,
+      }}
+    >
+      <Box
+        component="img"
+        loading="lazy"
+        src={imageSrc}
+        sx={{
+          width: { xs: '100%', md: '50%' },
+        }}
+      />
+      <Box
+        sx={{
+          position: 'relative',
+          width: { xs: '100%', md: '50%' },
+          minHeight: { xs: 0, md: 'unset' },
+          marginBottom: 4,
+        }}
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            left,
+            right,
+            top: { xs: -50, md: 'unset' },
+            paddingLeft: { xs: 0, md: 12 },
+            zIndex: 99,
+            backgroundColor: 'black.dark',
+            width: { xs: '90%', md: 'unset' },
+          }}
+        >
+          <Typography
+            variant="body1"
+            color="white.main"
+            sx={{ marginTop: { xs: 1, md: 0 } }}
+          >
+            {title}
+          </Typography>
+          <Typography
+            variant="h4"
+            color="white.main"
+            sx={{ marginTop: 1, marginBottom: 6, maxWidth: 460 }}
+          >
+            {description}
+          </Typography>
+          <Button
+            variant="outlined"
+            color="primary"
+            sx={{
+              minWidth: 'fit-content',
+              marginBottom: 6,
+            }}
+          >
+            {buttonTitle}
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
--- src/components/organisms/SalonsSection/SalonsSectionComponent.tsx
+++ src/components/organisms/SalonsSection/SalonsSectionComponent.tsx
@@ -1,6 +1,40 @@
 import { Box } from '@mui/material';
 import { FC } from 'react';
+import carSalons1Image from '~/assets/images/car-salons-1.png';
+import carSalons2Image from '~/assets/images/car-salons-2.png';
+import carSalons3Image from '~/assets/images/car-salons-3.png';
+import { Container } from '~/components/atoms';
+import { TextOverlayImage } from '~/components/molecules';
 
 export const SalonsSectionComponent: FC = () => {
-  return <Box>SalonsSection</Box>;
+  return (
+    <Box sx={{ backgroundColor: 'black.dark' }}>
+      <Container>
+        <Box height="fit-content">
+          <TextOverlayImage
+            fromSide="right"
+            imageSrc={carSalons1Image}
+            title="Technologia"
+            description="Poczuj pełną wolność jazdy dzięki licznym innowacjom."
+            buttonTitle="Umów jazdę próbną"
+          />
+          <TextOverlayImage
+            fromSide="left"
+            imageSrc={carSalons2Image}
+            title="Potęga Designu"
+            description="Crossover emanujący gracją, dynamizmem i czystym wyrafinowaniem."
+            buttonTitle="Umów jazdę próbną"
+          />
+          <TextOverlayImage
+            fromSide="right"
+            imageSrc={carSalons3Image}
+            title="Design wnętrza"
+            description="Idealny komfort. Unikalny wystrój wnętrza stworzony w stylu CUPRY."
+            buttonTitle="Umów jazdę próbną"
+          />
+          <Box sx={{ paddingBottom: { xs: 16, md: 0 } }} />
+        </Box>
+      </Container>
+    </Box>
+  );
 };
